ffa48b10b2dd0c3d9a4d0fc7cd4b4f48d2fa4844
giabao18 <phamgiabao1862001@gmail.com>
2021-11-22 18:24:31 +0700

    ex1

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -10,104 +10,407 @@
 #include <stdlib.h>
 #include <math.h>
 
+// void Ex1(int n)
+// {
+// 	int digit1 = n % 10000;
+// 	int digit2 = n % 1000;
+// 	int digit3 = n % 100;
+// 	int digit4 = n % 10;
+
+// 	switch (digit1)
+// 	{
+// 	case 1:
+// 		printf("one");
+// 		break;
+// 	case 2:
+// 		printf("two");
+// 		break;
+// 	case 3:
+// 		printf("three");
+// 		break;
+// 	case 4:
+// 		printf("four");
+// 		break;
+// 	case 5:
+// 		printf("five");
+// 		break;
+// 	case 6:
+// 		printf("six");
+// 		break;
+// 	case 7:
+// 		printf("seven");
+// 		break;
+// 	case 8:
+// 		printf("eight");
+// 		break;
+// 	case 9:
+// 		printf("nine");
+// 		break;
+// 	}
+// 	printf(" thousand ");
+
+// 	if (digit2 != 0)
+// 	{
+// 		switch (digit2)
+// 		{
+// 		case 1:
+// 			printf("one");
+// 			break;
+// 		case 2:
+// 			printf("two");
+// 			break;
+// 		case 3:
+// 			printf("three");
+// 			break;
+// 		case 4:
+// 			printf("four");
+// 			break;
+// 		case 5:
+// 			printf("five");
+// 			break;
+// 		case 6:
+// 			printf("six");
+// 			break;
+// 		case 7:
+// 			printf("seven");
+// 			break;
+// 		case 8:
+// 			printf("eight");
+// 			break;
+// 		case 9:
+// 			printf("nine");
+// 			break;
+// 		}
+// 		printf(" hundred ");
+// 	}
+
+// 	if (digit3 == 1)
+// 	{
+
+// 		switch (digit3)
+// 		{
+// 		case 1:
+// 			printf("eleven");
+// 			break;
+// 		case 2:
+// 			printf("twelve");
+// 			break;
+// 		case 3:
+// 			printf("thirteen");
+// 			break;
+// 		case 4:
+// 			printf("forteen");
+// 			break;
+// 		case 5:
+// 			printf("fifteen");
+// 			break;
+// 		case 6:
+// 			printf("sixteen");
+// 			break;
+// 		case 7:
+// 			printf("seventeen");
+// 			break;
+// 		case 8:
+// 			printf("eighteen");
+// 			break;
+// 		case 9:
+// 			printf("nineteen");
+// 			break;
+// 		}
+// 	}
+// 	else
+// 	{
+// 		switch (digit3)
+// 		{
+
+// 		case 2:
+// 			printf("twenty");
+// 			break;
+// 		case 3:
+// 			printf("thirty");
+// 			break;
+// 		case 4:
+// 			printf("forty");
+// 			break;
+// 		case 5:
+// 			printf("fifty");
+// 			break;
+// 		case 6:
+// 			printf("seventy");
+// 			break;
+// 		case 7:
+// 			printf("eighty");
+// 			break;
+// 		case 8:
+// 			printf("ninety");
+// 			break;
+// 		}
+// 		switch (digit4)
+// 		{
+
+// 		case 1:
+// 			printf("one");
+// 			break;
+// 		case 2:
+// 			printf("two");
+// 			break;
+// 		case 3:
+// 			printf("three");
+// 			break;
+// 		case 4:
+// 			printf("four");
+// 			break;
+// 		case 5:
+// 			printf("five");
+// 			break;
+// 		case 6:
+// 			printf("six");
+// 			break;
+// 		case 7:
+// 			printf("seven");
+// 			break;
+// 		case 8:
+// 			printf("eight");
+// 			break;
+// 		case 9:
+// 			printf("nine");
+// 			break;
+// 		}
+// 	}
+// }
+
 void Ex1(int n)
 {
-	// Your codes here
-	int t = 1000;
-	while (t == 1)
+	// Your code
+	while (n != 0)
 	{
-		/* code */
-		char string[50] = ("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "eleven", "twelve", "thirdteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety");
-		int value = n / t;
-		if (t > 10)
+		if (n >= 9000)
 		{
-			switch (value)
-			{
-			case 1:
-				printf("%c", string[0]);
-				break;
-			case 2:
-				printf("%c", string[1]);
-				break;
-			case 3:
-				printf("%c", string[2]);
-				break;
-			case 4:
-				printf("%c", string[3]);
-				break;
-			case 5:
-				printf("%c", string[4]);
-				break;
-			case 6:
-				printf("%c", string[5]);
-				break;
-			case 7:
-				printf("%c", string[6]);
-				break;
-			case 8:
-				printf("%c", string[7]);
-				break;
-			case 9:
-				printf("%c", string[8]);
-				break;
-			}
+			printf("nine thousand ");
+			n -= 9000;
 		}
-
-		if (t == 1000)
+		else if (n >= 8000)
 		{
-			printf(" thousand ");
+			printf("eight thousand ");
+			n -= 8000;
 		}
-
-		if (t == 100 && n / t != 0)
+		else if (n >= 7000)
 		{
-			printf(" hundred ");
+			printf("seven thousand ");
+			n -= 7000;
 		}
-
-		if (t == 10 && n / t != 0)
+		else if (n >= 6000)
 		{
-			if (value == 1)
-			{
-				int value2 = n % 1000;
-				switch (value2)
-				{
-				case 1:
-					printf("%c", string[9]);
-					break;
-				case 2:
-					printf("%c", string[10]);
-					break;
-				case 3:
-					printf("%c", string[11]);
-					break;
-				case 4:
-					printf("%c", string[12]);
-					break;
-				case 5:
-					printf("%c", string[13]);
-					break;
-				case 6:
-					printf("%c", string[14]);
-					break;
-				case 7:
-					printf("%c", string[15]);
-					break;
-				case 8:
-					printf("%c", string[16]);
-					break;
-				case 9:
-					printf("%c", string[17]);
-					break;
-				}
-			}
-			else
-			{
-			}
+			printf("six thousand ");
+			n -= 6000;
 		}
-
-		t /= 10;
+		else if (n >= 5000)
+		{
+			printf("five thousand ");
+			n -= 5000;
+		}
+		else if (n >= 4000)
+		{
+			printf("four thousand ");
+			n -= 4000;
+		}
+		else if (n >= 3000)
+		{
+			printf("three thousand ");
+			n -= 3000;
+		}
+		else if (n >= 2000)
+		{
+			printf("two thousand ");
+			n -= 2000;
+		}
+		else if (n >= 1000)
+		{
+			printf("one thousand ");
+			n -= 1000;
+		}
+		else if (n >= 900)
+		{
+			printf("nine hundred ");
+			n -= 900;
+		}
+		else if (n >= 800)
+		{
+			printf("eight hundred ");
+			n -= 800;
+		}
+		else if (n >= 700)
+		{
+			printf("seven hundred ");
+			n -= 700;
+		}
+		else if (n >= 600)
+		{
+			printf("six hundred ");
+			n -= 600;
+		}
+		else if (n >= 500)
+		{
+			printf("five hundred ");
+			n -= 500;
+		}
+		else if (n >= 400)
+		{
+			printf("four hundred ");
+			n -= 400;
+		}
+		else if (n >= 300)
+		{
+			printf("three hundred ");
+			n -= 300;
+		}
+		else if (n >= 200)
+		{
+			printf("two hundred ");
+			n -= 200;
+		}
+		else if (n >= 100)
+		{
+			printf("one hundred ");
+			n -= 100;
+		}
+		else if (n >= 90)
+		{
+			printf("ninety ");
+			n -= 90;
+		}
+		else if (n >= 80)
+		{
+			printf("eighty ");
+			n -= 80;
+		}
+		else if (n >= 70)
+		{
+			printf("seventy ");
+			n -= 70;
+		}
+		else if (n >= 60)
+		{
+			printf("sixty ");
+			n -= 60;
+		}
+		else if (n >= 50)
+		{
+			printf("fifty ");
+			n -= 50;
+		}
+		else if (n >= 40)
+		{
+			printf("fourty ");
+			n -= 40;
+		}
+		else if (n >= 30)
+		{
+			printf("thirty ");
+			n -= 30;
+		}
+		else if (n >= 20)
+		{
+			printf("twenty ");
+			n -= 20;
+		}
+		else if (n >= 19)
+		{
+			printf("nineteen");
+			n -= 19;
+		}
+		else if (n >= 18)
+		{
+			printf("eighteen");
+			n -= 18;
+		}
+		else if (n >= 17)
+		{
+			printf("seventeen");
+			n -= 17;
+		}
+		else if (n >= 16)
+		{
+			printf("sixteen");
+			n -= 16;
+		}
+		else if (n >= 15)
+		{
+			printf("fifteen");
+			n -= 15;
+		}
+		else if (n >= 14)
+		{
+			printf("fourteen");
+			n -= 14;
+		}
+		else if (n >= 13)
+		{
+			printf("thirteen");
+			n -= 13;
+		}
+		else if (n >= 12)
+		{
+			printf("twelve");
+			n -= 12;
+		}
+		else if (n >= 11)
+		{
+			printf("eleven");
+			n -= 11;
+		}
+		else if (n >= 10)
+		{
+			printf("ten ");
+			n -= 10;
+		}
+		else if (n >= 9)
+		{
+			printf("nine");
+			n -= 9;
+		}
+		else if (n >= 8)
+		{
+			printf("eight");
+			n -= 8;
+		}
+		else if (n >= 7)
+		{
+			printf("seven");
+			n -= 7;
+		}
+		else if (n >= 6)
+		{
+			printf("six");
+			n -= 6;
+		}
+		else if (n >= 5)
+		{
+			printf("five");
+			n -= 5;
+		}
+		else if (n >= 4)
+		{
+			printf("four");
+			n -= 4;
+		}
+		else if (n >= 3)
+		{
+			printf("three");
+			n -= 3;
+		}
+		else if (n >= 2)
+		{
+			printf("two");
+			n -= 2;
+		}
+		else if (n >= 1)
+		{
+			printf("one");
+			n -= 1;
+		}
 	}
 }
-
 int main(int argc, char *argv[])
 {
 	// testing variable, applying it to your algorithm for auto-evaluating
